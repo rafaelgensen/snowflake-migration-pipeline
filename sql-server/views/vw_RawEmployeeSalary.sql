9192e344b2cb23608ae81569a80c91c0f47ae85e
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-30 14:05:05 -0300

    docs: sql server / first commit

--- sql-server/views/vw_RawEmployeeSalary.sql
+++ sql-server/views/vw_RawEmployeeSalary.sql
@@ -1,13 +1,14 @@
-CREATE VIEW HR.vw_RawEmployeeSalary
-AS
+CREATE VIEW HR.vw_RawEmployeeSalary AS
 SELECT 
     e.EmployeeID,
     e.FirstName,
     e.LastName,
-    e.Department,
+    d.Name AS Department,
     s.Amount,
     s.EffectiveDate,
     s.EndDate
 FROM HR.Employees e
 JOIN HR.Salaries s
-    ON e.EmployeeID = s.EmployeeID;
+    ON e.EmployeeID = s.EmployeeID
+JOIN HR.Departments d
+    ON e.DepartmentID = d.DepartmentID;
